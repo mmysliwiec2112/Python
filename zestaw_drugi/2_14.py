2c9f02e6c9bdfbce9a7103f02f6cb49983119b89
mmysliwiec2112 <66027053+mmysliwiec2112@users.noreply.github.com>
2021-10-15 18:31:22 +0200

    Rozwiązanie zadania 2.14

--- zestaw_drugi/2_14.py
+++ zestaw_drugi/2_14.py
@@ -1,8 +1,9 @@
 line = "Lorem ipsum \n doloro sit amet"
 # I added 'o' in word 'dolor' to get one longest word with length of 6
+
 line = line.split()
 length = [len(line[each]) for each in range(0, len(line))]
 for each in range(0, len(line)):
     if max(length) == len(line[each]):
-        print(line[each])
-print(max(length))
+        print(f'Longest word in the line: {line[each]}')
+print(f'Length of the longest word in the line: {max(length)}')
