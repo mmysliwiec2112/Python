c114c0f9e9d8ae0f977fb50615f7d90f40b26358
mmysliwiec2112 <66027053+mmysliwiec2112@users.noreply.github.com>
2021-10-15 18:43:56 +0200

    Rozwiązanie zadania 2.18

--- zestaw_drugi/2_18.py
+++ zestaw_drugi/2_18.py
@@ -1,5 +1,5 @@
-# 7 zer
+# 7 zeroes
 number = 1890293016270560023030
 number_as_str = str(number)
-print(number_as_str.count('0'))
+print(f'Number of zeroes in a large number: {number_as_str.count("0")}')
 
