bed899e342c270be415943f4a8cb2483577cd636
mmysliwiec2112 <66027053+mmysliwiec2112@users.noreply.github.com>
2021-11-15 23:54:12 +0100

    Rozwiązanie do zestawu 5-tego

--- zestaw_piaty/fracs.py
+++ zestaw_piaty/fracs.py
@@ -1,38 +1,38 @@
 import math
 
 
+def red_frac(frac):
+    if frac[1] == 0:
+        print('Nie można dzielić przez zero!')
+        return
+    gcd = math.gcd(frac[0], frac[1])
+    frac = [frac[0] / gcd, frac[1] / gcd]
+
+    if frac[0] > 0 > frac[1]:
+        frac = [frac[0] * (-1), frac[1] * (-1)]
+    return frac
+
+
 def add_frac(frac1, frac2):
     if frac1[1] == frac2[1]:
-        return [frac1[0] + frac2[0], frac1[1] + frac2[1]]
+        return [frac1[0] + frac2[0], frac1[1]]
     else:
-        n_frac1 = [frac1[0] * frac2[1], frac1[1] * frac2[1]]
-        n_frac2 = [frac1[1] * frac2[0], frac1[1] * frac2[1]]
-        result = [n_frac1[0] + n_frac2[0], n_frac1[1]]
-        gcd = math.gcd(result[0], result[1])
-        return [result[0] / gcd, result[1] / gcd]
+        return red_frac([frac1[0] * frac2[1] + frac2[0] * frac1[1], frac1[1] * frac2[1]])
 
 
 def sub_frac(frac1, frac2):
     if frac1[1] == frac2[1]:
         return [frac1[0] - frac2[0], frac1[1] - frac2[1]]
     else:
-        n_frac1 = [frac1[0] * frac2[1], frac1[1] * frac2[1]]
-        n_frac2 = [frac1[1] * frac2[0], frac1[1] * frac2[1]]
-        result = [n_frac1[0] - n_frac2[0], n_frac1[1]]
-        gcd = math.gcd(result[0], result[1])
-        return [result[0] / gcd, result[1] / gcd]
+        return red_frac([frac1[0] * frac2[1] - frac2[0] * frac1[1], frac1[1] * frac2[1]])
 
 
 def mul_frac(frac1, frac2):
-    result = [frac1[0] * frac2[0], frac1[1] * frac2[1]]
-    gcd = math.gcd(result[0], result[1])
-    return [result[0] / gcd, result[1] / gcd]
+    return red_frac([frac1[0] * frac2[0], frac1[1] * frac2[1]])
 
 
 def div_frac(frac1, frac2):
-    result = [frac1[0] * frac2[1], frac1[1] * frac2[0]]
-    gcd = math.gcd(result[0], result[1])
-    return [result[0] / gcd, result[1] / gcd]
+    return red_frac([frac1[0] * frac2[1], frac1[1] * frac2[0]])
 
 
 def is_positive(frac):
@@ -61,6 +61,10 @@ def cmp_frac(frac1, frac2):
 
 
 def frac2float(frac): return frac[0] / frac[1]
+
+
+def dividing_by_zero_err():
+    print("Nie można dzielić przez zero!")
 
 
 f1 = [-1, 2]  # -1/2
--- zestaw_piaty/test_fracs.py
+++ zestaw_piaty/test_fracs.py
@@ -8,27 +8,62 @@ class TestFractions(unittest.TestCase):
 
     def test_add_frac(self):
         self.assertEqual(fracs.add_frac([1, 2], [1, 3]), [5, 6])
+        self.assertEqual(fracs.add_frac([1, 3], [1, 4]), [7, 12])
+        self.assertEqual(fracs.add_frac([-1, 2], [1, 3]), [-1, 6])
+        self.assertEqual(fracs.add_frac([1, 2], [-1, 3]), [1, 6])
+        self.assertEqual(fracs.add_frac([1, 3], [1, 3]), [2, 3])
+
 
     def test_sub_frac(self):
         self.assertEqual(fracs.sub_frac([1, 2], [1, 3]), [1, 6])
+        self.assertEqual(fracs.sub_frac([1, 3], [1, 4]), [1, 12])
+        self.assertEqual(fracs.sub_frac([-1, 2], [1, 3]), [-5, 6])
+        self.assertEqual(fracs.sub_frac([1, 2], [-1, 3]), [5, 6])
+        self.assertEqual(fracs.sub_frac([1, 3], [1, 3]), [0, 0])
 
     def test_mul_frac(self):
         self.assertEqual(fracs.mul_frac([1, 2], [1, 3]), [1, 6])
+        self.assertEqual(fracs.mul_frac([1, 3], [1, 4]), [1, 12])
+        self.assertEqual(fracs.mul_frac([-1, 2], [1, 3]), [-1, 6])
+        self.assertEqual(fracs.mul_frac([1, 2], [-1, 3]), [-1, 6])
+        self.assertEqual(fracs.mul_frac([1, 3], [1, 3]), [1, 9])
 
     def test_div_frac(self):
         self.assertEqual(fracs.div_frac([1, 2], [1, 3]), [3, 2])
+        self.assertEqual(fracs.div_frac([1, 3], [1, 4]), [4, 3])
+        self.assertEqual(fracs.div_frac([-1, 2], [1, 3]), [-3, 2])
+        self.assertEqual(fracs.div_frac([1, 2], [-1, 3]), [-3, 2])
+        self.assertEqual(fracs.div_frac([1, 3], [1, 3]), [1, 1])
+
 
     def test_is_positive(self):
         self.assertEqual(fracs.is_positive([1, 2]), 1)
+        self.assertEqual(fracs.is_positive([1, -2]), 0)
+        self.assertEqual(fracs.is_positive([-1, 2]), 0)
+        self.assertEqual(fracs.is_positive([-1, -2]), 0)
 
     def test_is_zero(self):
         self.assertEqual(fracs.is_zero([1, 2]), 0)
+        self.assertEqual(fracs.is_zero([0, 2]), 1)
+        self.assertEqual(fracs.is_zero([1, 0]), 0)
+        self.assertEqual(fracs.is_zero([0, 0]), 0)
 
     def test_cmp_frac(self):
         self.assertEqual(fracs.cmp_frac([1, 2], [1, 3]), 1)
+        self.assertEqual(fracs.cmp_frac([1, 2], [1, 2]), 0)
+        self.assertEqual(fracs.cmp_frac([1, 3], [1, 2]), -1)
+        self.assertEqual(fracs.cmp_frac([1, 4], [1, 32]), 1)
+        self.assertEqual(fracs.cmp_frac([1, 32], [1, 32]), 0)
+        self.assertEqual(fracs.cmp_frac([1, 32], [1, 4]), -1)
+
 
     def test_frac2float(self):
         self.assertEqual(fracs.frac2float([1, 2]), 1/2)
+        self.assertEqual(fracs.frac2float([-1, 2]), -1/2)
+        self.assertEqual(fracs.frac2float([1, 1]), 1)
+        self.assertEqual(fracs.frac2float([3, 2]), 3/2)
+        self.assertEqual(fracs.frac2float([1, -2]), -1/2)
+
 
     def tearDown(self): pass
 
