d70794a4c16010fae59f932db27069a5169e0754
mmysliwiec2112 <66027053+mmysliwiec2112@users.noreply.github.com>
2022-02-11 12:41:02 +0100

    Add files via upload

--- projekt/needlemanWunsch.py
+++ projekt/needlemanWunsch.py
@@ -1,10 +1,12 @@
 import numpy as np
 
+
 def score(val_x, val_y, match, mismatch):
     if val_x == val_y:
         return match
     else:
         return -mismatch
+
 
 def calculate_scores(seq_x, seq_y, match=1, mismatch=3, gap=4):
     len_x = len(seq_x)
@@ -14,6 +16,13 @@ def calculate_scores(seq_x, seq_y, match=1, mismatch=3, gap=4):
     grid = np.zeros((len_x + 1, len_y + 1))
     grid[:, 0] = np.linspace(0, -len_x * gap, len_x + 1)
     grid[0, :] = np.linspace(0, -len_y * gap, len_y + 1)
+
+    traceback_matrix = [[[] for j in range(len_y + 1)] for i in range(len_x + 1)]
+
+    for i in range(len_x):
+        traceback_matrix[i][0] += 'l'
+    for i in range(len_y):
+        traceback_matrix[0][i] += 'u'
 
     # Calculating scores.
     temporary_scores = []
@@ -25,73 +34,103 @@ def calculate_scores(seq_x, seq_y, match=1, mismatch=3, gap=4):
             temporary_scores.append(grid[i, j - 1] - gap)
             tmax = np.max(temporary_scores)
             grid[i, j] = tmax
+
+            # adding proper values to the traceback matrix\
+            if temporary_scores[0] == tmax:
+                traceback_matrix[i][j] += 'd'
+            elif temporary_scores[1] == tmax:
+                traceback_matrix[i][j] += 'l'
+            elif temporary_scores[2] == tmax:
+                traceback_matrix[i][j] += 'u'
             temporary_scores = []
 
     alignment_x = ""
     alignment_y = ""
+    control = ''
     i = len(seq_x)
     j = len(seq_y)
     while i > 0 or j > 0:
-        if i > 0 and j > 0 and grid[i, j] == (grid[i-1, j-1] + score(seq_x[i - 1], seq_y[j - 1], match, mismatch)):
+        if traceback_matrix[i][j] == ['d']:
             alignment_x += seq_x[i - 1]
             alignment_y += seq_y[j - 1]
+            control += 'd'
             i -= 1
             j -= 1
-        elif i > 0 and grid[i, j] == (grid[i-1, j] - gap):
+        elif traceback_matrix[i][j] == ['l']:
             alignment_x += seq_x[i - 1]
             alignment_y += '-'
+            control += 'l'
             i -= 1
-        else:
+        elif traceback_matrix[i][j] == ['u']:
             alignment_x += '-'
             alignment_y += seq_y[j - 1]
+            control += 'u'
             j -= 1
     alignment_x = ''.join(alignment_x)[::-1]
     alignment_y = ''.join(alignment_y)[::-1]
+    print(control)
     return '\n'.join([alignment_x, alignment_y])
 
 
+def print_results(x, y):
+    str_x = ''
+    for i in range(len(x)):
+        str_x += (x[i])
 
-x = "GATTACA"
-y = "GCATGCU"
-print(calculate_scores(x, y))
-# GATTACA
-# GCATGCU
+    str_y = ''
+    for i in range(len(y)):
+        str_y += (y[i])
+    print(f'\n\nalignment for: \nx: {str_x}\ny: {str_y}\n{calculate_scores(x, y)}')
 
-np.random.seed(42)
+import unittest
 
-x = np.random.choice(['A', 'T', 'G', 'C'], 10)
-y = np.random.choice(['A', 'T', 'G', 'C'], 10)
 
-str_x = ''
-for i in range(len(x)):
-    str_x += (x[i])
+class TestList(unittest.TestCase):
 
-str_y = ''
-for i in range(len(y)):
-    str_y += (y[i])
+    def setUp(self): pass
 
-print(str_x)
-print(str_y)
+#wyniki testów sprawdzane przy pomocy strony https://bioboot.github.io/bimm143_W20/class-material/nw/
+    def testCalculateScores(self):
+        x = "GATTACA"
+        y = "GCATGCU"
+        self.assertTrue(calculate_scores(x, y), ["GATTACA", "GCATGCU"])
 
-print(calculate_scores(x, y))
+        np.random.seed(32)
+        x = np.random.choice(['A', 'T', 'G', 'C'], 10)
+        y = np.random.choice(['A', 'T', 'G', 'C'], 5)
+        self.assertTrue(calculate_scores(x, y), ["CCTGAGACAC", "-----GTCTG"])
 
-x = np.random.choice(['A', 'T', 'G', 'C'], 50)
-y = np.random.choice(['A', 'T', 'G', 'C'], 50)
+        np.random.seed(32)
+        x = np.random.choice(['A', 'T', 'G', 'C'], 10)
+        y = np.random.choice(['A', 'T', 'G', 'C'], 10)
+        self.assertTrue(calculate_scores(x, y), ["-CCTGAGACAC", "GTCTG-TACTC"])
 
-for i in range(len(x)):
-    str_x += (x[i])
+        np.random.seed(42)
+        x = np.random.choice(['A', 'T', 'G', 'C'], 10)
+        y = np.random.choice(['A', 'T', 'G', 'C'], 10)
+        self.assertTrue(calculate_scores(x, y), ["GCAGGCA-AGT", "G-GGGCACCCG"])
 
-for i in range(len(y)):
-    str_y += (y[i])
+    def tearDown(self): pass
 
-print(str_x)
-print(str_y)
 
-print(calculate_scores(x, y))
+if __name__ == '__main__':
 
-print(calculate_scores(x, y, gap=0))
+    np.random.seed(42)
+    x = np.random.choice(['A', 'T', 'G', 'C'], 50)
+    y = np.random.choice(['A', 'T', 'G', 'C'], 50)
+    print_results(x, y)
 
-print(calculate_scores(x, y, gap=10))
+    print('\nresults for gap value equal to 0:')
+    print(calculate_scores(x, y, gap=0))
 
-print(calculate_scores(x, y, mismatch=5))
+    print('\nresults for gap value equal to 10:')
+    print(calculate_scores(x, y, gap=10))
+
+    print('\nresults for mismatch value equal to 5:')
+    print(calculate_scores(x, y, mismatch=5))
+
+    unittest.main()
+
+
+
 
